6aaa934667aa632ad5314c48ce31ac29896e2c84
Kauê Evangelista da Silva Aguiar <kauesilva38@hotmail.com>
2024-04-02 11:01:09 -0300

    comit

--- App.js
+++ App.js
@@ -1,7 +1,50 @@
 import { Button, StatusBar, StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import * as Notifications from "expo-notifications";
+
+//Manipulador de eventos de notificação
+Notifications.setNotificationHandler({
+  handleNotification: (async = () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    };
+  }),
+});
 
 export default function App() {
-  const enviarMensagem = () => {};
+  const [dados, setDados] = useState(null);
+
+  useEffect(() => {
+    /* Verific */
+    async function permissoesIos() {
+      return Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowBadge: true,
+          allowSound: true,
+        },
+      });
+    }
+    permissoesIos();
+
+    /* o que fazer quando a notificação for recebida no app */
+    Notifications.addNotificationReceivedListener((notifcacao) => {
+      console.log(notifcacao);
+    });
+
+    /* evento de resposta para dadas às notificações, ou seja, qunado o usuário interage(toca) na notificação */
+    Notifications.addNotificationReceivedListener((resposta) => {
+      console.log(resposta);
+    });
+  }, []);
+
+  const enviarMensagem = () => {
+    /* montar a mensagem que será enviada via sistema de notificações */
+    const mensagem = {};
+  };
+
   return (
     <>
       <StatusBar />
